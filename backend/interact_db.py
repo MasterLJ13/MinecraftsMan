da8c1d82eb9a60a5e8e5f161298f1cc1bb36ae7c
lara <lara.dvorsek@gmail.com>
2023-11-18 20:57:30 +0100

    renaming

--- backend/interact_db.py
+++ backend/interact_db.py
@@ -1,5 +1,4 @@
 import sqlite3
-import sys
 from sqlite3 import Error
 
 
@@ -30,7 +29,11 @@ def query_postcode_infos(db_file, postalcode):
     cursor = conn.cursor()
 
     # Get longitude, latitude and group of a postalcode
-    cursor.execute(f"SELECT lat, lon, postcode_extension_distance_group FROM postcode WHERE postcode={postalcode}")
+    cursor.execute(f"""
+                   SELECT lat, lon, postcode_extension_distance_group
+                   FROM postcode 
+                   WHERE postcode={postalcode}
+                   """)
     post_lat, post_lon, group = cursor.fetchone()
 
     # Close the cursor and connection
@@ -45,34 +48,33 @@ def query_ranking(db_file, post_lon, post_lat, group):
     con = sqlite3.connect(db_file)
     cursor = con.cursor()
 
-    r = 6371  # radius of earth in km
-
+    r = 6371  # Radius of earth in km
     postcode_extension_distance_bonus = {'a': 0, 'b': 2, 'c': 5}[group[-1]]
 
     query = f"""
-            WITH MALER_DIST as (
+            WITH CRAFTSMEN_DIST as (
                 SELECT *, abs((acos((sin(lat)*sin({post_lat})) + (cos(lat) * cos({post_lat}) * cos(lon - {post_lon})) )*{r})) as dist
                 FROM service_provider_profile
             ),
-            NEAR_MALER_DIST as (
+            NEAR_CRAFTSMEN_DIST as (
                 SELECT *
-                FROM MALER_DIST
+                FROM CRAFTSMEN_DIST
                 WHERE dist < CAST(((max_driving_distance/1000) + {postcode_extension_distance_bonus}) AS float)
             ),
-            NEAR_MALER_WITH_PROFILE_SCORE as(
+            NEAR_CRAFTSMEN_WITH_PROFILE_SCORE as(
                 SELECT m.*, (0.4 * q.profile_picture_score) + (0.6 * q.profile_description_score) as profile_score
-                FROM NEAR_MALER_DIST m JOIN quality_factor_score q ON m.id = q.profile_id
+                FROM NEAR_CRAFTSMEN_DIST m JOIN quality_factor_score q ON m.id = q.profile_id
             ),
-            NEAR_MALER_WITH_RANK_HELPER as(
+            NEAR_CRAFTSMEN_WITH_RANK_HELPER as(
                 SELECT *, 1-(dist/80) as dist_score, CASE WHEN dist>80 THEN 0.01 ELSE 0.15 END as dist_weight
-                FROM NEAR_MALER_WITH_PROFILE_SCORE
+                FROM NEAR_CRAFTSMEN_WITH_PROFILE_SCORE
             ),
-            NEAR_MALER_WITH_RANK as (
+            NEAR_CRAFTSMEN_WITH_RANK as (
                 SELECT *, (dist_weight * dist_score + (1 - dist_weight) * profile_score) as rankingScore
-                FROM NEAR_MALER_WITH_RANK_HELPER
+                FROM NEAR_CRAFTSMEN_WITH_RANK_HELPER
             )
             SELECT id, first_name || ' ' || last_name as name, rankingScore
-            FROM NEAR_MALER_WITH_RANK
+            FROM NEAR_CRAFTSMEN_WITH_RANK
             ORDER BY rankingScore desc
             LIMIT 20
         """
